b0d769bd0e2cb075632bdaf28bff999e4331a3e0
M Banks <montensia1@outlook.com>
2019-06-10 15:13:29 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,39 @@
-
+require 'pry'
+class CashRegister
+  attr_accessor :total, :discount, :title, :price, :quantity
+  
+ # @@items = []
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @items = []
+  end
+  
+  def add_item(title, price, quantity = 0)
+    #binding.pry
+    #@title = title
+    @items << title
+    @quantity = quantity
+    if @quantity > 0
+      price_increased = price * quantity
+      @total += price_increased
+    else
+      @total += price
+    end
+  end
+  
+  def apply_discount
+    if @discount > 0
+      discounted_rate = @total * self.discount * 0.010
+      @total -= discounted_rate 
+      "After the discount, the total comes to $#{(@total).round}."
+    else
+      "There is no discount to apply."
+    end
+  end
+  
+  def items
+    @items
+  end
+  
+end
